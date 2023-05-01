c79057e9dc8ed766c7ee655997660b93b118bc45
Mike Zwilling <mikezwilling@hotmail.com>
2023-05-01 15:30:01 -0700

    Extract MSCCL attributes, other improvements

--- bench.py
+++ bench.py
@@ -4,18 +4,32 @@ import re
 import datetime
 import fnmatch
 import argparse
+import xml.etree.ElementTree as ET
 
 
-# This python program is runs NCCL benchmarks tests using NCCL and MSCCL
-# It takes 4 command line arguments: --mode, --directory, --filter, --outputDirectory, --endBufSize, --nGPU
-#  mode: is either "test" or "run" where "test" indicates that the program should run in test mode and
-#       "run" indicates that the program should actually run the benchmarks.
-#  directory: is the directory where the MSCCL xml files are located
-#  filter: is a filter in the style of https://docs.python.org/3/library/fnmatch.html for files in the directory that will be tested
-#  outputDirectory: is the directory where the output files will be placed
-#     The file "results.txt" in the output directory will have results appended to it. It will be created if it doesn't exist
-#  endBufSize: is the end buffer size to use for the test. It is optional and defaults to 32MB.
-#  nGPU: is the number of GPUs to use for the test. It is optional and defaults to 16.
+# This python program is runs NCCL benchmarks tests (https://github.com/NVIDIA/nccl-tests)
+# with and without MSCCL enabled and appends the results to a CSV file for further analysis.
+# Prerequisites:
+# 1. Install python 3.6 or greater
+# 2. Clone and build the NCCL test repository https://github.com/NVIDIA/nccl-tests.
+# 3. Clone and build the MSCCL repository https://github.com/microsoft/msccl.git
+# 4. Install MSCCL tools https://github.com/microsoft/msccl-tools.git
+# 5. Use MSCCL tools to generate the MSCCL xml files
+#    or use pre-generated xml files from https://github.com/parasailteam/sccl-presynth/tree/main/sccl_presynth
+#
+# Usage: this program takes the following arguments: --mode, --directory, --filter, --outputDirectory, --endBufSize
+# See the main function for more details about the arguments.
+# Example command line: python bench.py --mode run --directory sccl-presynth/sccl_presynth --filter *gather.n16* --output output
+#
+# Details on "--mode test":
+# The "--mode test" option is used to test the program without actually running the benchmarks.
+# It will still iterate overall of the files in the directory and filter but it will not run the benchmarks.
+# The file contents do not matter as the variable exampleMCCLInput is used instead of the file contents.
+# The output is also faked with the results being the variable exampleOutput.
+#
+# TODO comments are scattered throughout the code to indicate areas that need improvement.
+#   Primarily these are hard-coded constants that should probably be command line arguments.
+#   Such as the separator for the CSV file.
 #
 def main():
     # Setup an argument parser to parse the command line arguments
@@ -25,7 +39,6 @@ def main():
     parser.add_argument('-f', '--filter', type=str, help='filter: is a filter in the style of https://docs.python.org/3/library/fnmatch.html for files in the directory that will be tested', required=True)
     parser.add_argument('-o', '--outputDirectory', type=str, help='outputDirectory: is the directory where the output files will be placed', required=True)
     parser.add_argument('-e', '--endBufSize', type=str, help='endBufSize: is the end buffer size to use for the test. It is optional and defaults to 32MB.', default="32MB")
-    parser.add_argument('-n', '--nGPU', type=int, help='nGPU: is the number of GPUs to use for the test. It is optional and defaults to 16.', default=16)
 
     # Parse the command line arguments
     args = parser.parse_args()
@@ -41,10 +54,13 @@ def main():
     filter = args.filter
     outputDirectory = args.outputDirectory
     endBufSize = args.endBufSize
-    nGPU = args.nGPU
 
     # Print all of the command line arguments
     print(f"Arguments: {args}")
+
+    # Separator to use in the CSV result file
+    # TODO: consider making this a command line argument
+    separator = "\t"
 
     # This is the CSV file with the final results
     resultFileName = "results.txt"
@@ -56,14 +72,15 @@ def main():
 
     # Make sure a file named resultFile exists in outputDirectory and if it doesn't create it.
     # Make sure the file path works for both linux and Windows
-    resultFile = os.path.join(outputDirectory, resultFileName);
+    resultFile = os.path.join(outputDirectory, resultFileName)
     if not os.path.exists(resultFile):
         print("Creating result file: " + resultFile)
         # Put header row into the result file
         with open(resultFile, "w") as f:
-            f.writelines(f'Algorithm,Filter,TimeOfTest,Library,NcclVersion,Size,Time_OutOfPlace,Time_InPlace,MSCCL_File\n')
+            f.writelines(format_header_row(separator))
 
     prossessedFiles = []
+
 
     # get current date and time as a string
     timeOfTest = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -74,17 +91,19 @@ def main():
     # For each file filename in directory that matches the filter in the name do the following:
     for filename in os.listdir(directory):
         if fnmatch.fnmatch(filename, filter):
-            # if the file name doesn't contain the number of GPUs specified in the command line arguments then skip it
-            if str(nGPU) not in filename:
-                print(f"Skipping file {filename} because it doesn't contain the number of GPUs specified in the command line arguments")
-                continue
 
             # Track the names of all files processed from the directory
             prossessedFiles.append(filename)
 
-            outputFileList = runBenchmark(mode, filename, endBufSize, directory, outputDirectory)
+            # Parse the XML file to get the algorithm attributes for the benchmark
+            # Example: algo {'name': 'Allgather(n=16)-Distributed...', 'nchannels': '8', 'nchunksperloop': '128', 'proto': 'Simple', 'ngpus': '16', 'coll': 'allgather', 'inplace': '1'}
+            if (mode == "test"):
+                algorithmAttributes = parse_msccl_xml(exampleMCCLInput)
+            else:
+                # Parse the XML file to get the algorithm attributes for the benchmark. Input is the contents of the file.
+                algorithmAttributes = parse_msccl_xml(open(os.path.join(directory, filename), "r").read())
 
-            algorithm = filename.split(".")[0]
+            outputFileList = run_benchmark(mode, algorithmAttributes, filename, endBufSize, directory, outputDirectory)
 
             # For each file in outputFileList parse the file to find the results
             # And append them to the resultFile in CSV format
@@ -97,10 +116,21 @@ def main():
                             break
 
                 # Parse the output file to get each line of performance results
-                data = parse_test_log(outputFile)
+                data = parse_test_log(outputFile, separator)
                 for line in data:
                     library = "MSCCL" if "msccl" in outputFile else "NCCL"
-                    resultsF.write(f'{algorithm},{filter},{timeOfTest},{library},{ncclVersion},{line},{filename}\n')
+                    # Write the results to the result file in CSV format.
+                    resultsF.write(algorithmAttributes.get("coll")
+                        + separator + filter
+                        + separator + timeOfTest
+                        + separator + algorithmAttributes.get("ngpus")
+                        + separator + algorithmAttributes.get("proto")
+                        + separator + library
+                        + separator + ncclVersion
+                        + separator + line # line is a string with Size, Time_OutOfPlace, Time_InPlace in CSV format.
+                        + separator + algorithmAttributes.get("name")
+                        + separator + filename
+                        + '\n')
 
     resultsF.close()
 
@@ -113,13 +143,14 @@ def main():
 
 # Execute the test program on the file filename, place results in the output folder
 # then returns a list of the names of the output files
-def runBenchmark(mode, filename, endBufSize, inputDirectory, outputDirectory):
+def run_benchmark(mode, algorithmAttributes, filename, endBufSize, inputDirectory, outputDirectory) -> list:
     print(f"\nRun Benchmark in mode {mode} for file: " + filename)
 
+    # The list of output files that will be returned (one for NCCl and one for MSCCL)
     outputFileList = []
 
-    # Extract the substring from the name of the file up until the first "." in the file name. Call this substring "algorithm"
-    mscclAlgorithm = filename.split(".")[0]
+    # Extract the substring name of the algorithm up until the first "." in the file name. Call this substring "algorithm"
+    mscclCollective = algorithmAttributes.get("coll")
 
     # Construct the string name of the nccl algorithm from the msccl algorithm.
     # Nccl uses "_" to separate words in the algorithm name and msccl names are more insconsistent.
@@ -136,14 +167,14 @@ def runBenchmark(mode, filename, endBufSize, inputDirectory, outputDirectory):
     #            ncclAlgorithm = "all_to_all"
     #        case _:
     #            raise Exception("Unknown algorithm: " + mscclAlgorithm)
-    if mscclAlgorithm.lower() == "allreduce":
+    if mscclCollective.lower() == "allreduce":
         ncclAlgorithm = "all_reduce"
-    elif mscclAlgorithm.lower() == "allgather":
+    elif mscclCollective.lower() == "allgather":
         ncclAlgorithm = "all_gather"
-    elif mscclAlgorithm.lower() == "alltoall":
+    elif mscclCollective.lower() == "alltoall":
         ncclAlgorithm = "all_to_all"
     else:
-        raise Exception("Unknown algorithm: " + mscclAlgorithm)
+        raise Exception("Unexpected algorithm: " + mscclAlgorithmNamePrefix)
 
     ncclPerfTest = ncclAlgorithm + "_perf"
 
@@ -157,7 +188,9 @@ def runBenchmark(mode, filename, endBufSize, inputDirectory, outputDirectory):
     numProcs = " -np 16"  # for the -np parameter
     mcaParams = " -hostfile /job/hostfile -mca pml ob1 -mca btl ^openib -mca btl_tcp_if_include eth0"
 
-    # The environment variables that are set for the command - consider separating these out variables that are non-constant
+    # The environment variables that are set for the command
+    # TODO: Consider separating these out variables that are non-constant. Originally the path prefix
+    #    was ~/ but seemed to have problems, so I changed it to the full path.
     envVars = " -x PATH -x LD_LIBRARY_PATH=/home/saemal/msccl/build/lib/:$LD_LIBRARY_PATH -x UCX_IB_ENABLE_CUDA_AFFINITY=n -x NCCL_IB_PCI_RELAXED_ORDERING=1 -x UCX_IB_PCI_RELAXED_ORDERING=on -x UCX_NET_DEVICES=mlx5_0:1 -x UCX_TLS=rc -x NCCL_SOCKET_IFNAME=eth0 -x NCCL_DEBUG=INFO -x NCCL_NET_GDR_LEVEL=5 -x NCCL_DEBUG_SUBSYS=INIT,ENV -x NCCL_ALGO=MSCCL,RING,TREE -x CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7"
 
     mscclEnvVars = " -x MSCCL_XML_FILES=" + os.path.join(inputDirectory, filename)
@@ -179,7 +212,6 @@ def runBenchmark(mode, filename, endBufSize, inputDirectory, outputDirectory):
     else:
         assert mode == "run"
         os.system(f"{ncclCommandLine}")
-
 
     # Run the MSCCL version of the benchmark and direct the output to a new file called filename_msccl_result
     # Delete the output file if it already exists
@@ -216,10 +248,25 @@ def runBenchmark(mode, filename, endBufSize, inputDirectory, outputDirectory):
     # Return the list of successful benchmark results
     return outputFileList
 
+# Parse MSCCL XML file content and return a dictionary of attributes for the algo element in the file:
+#   - algo: algorithm name
+#   - proto: protocol name
+#   - ngpus: number of GPUs
+#   - coll: collective name
+#   - inplace: boolean for in-place or out-of-place
+# See example input from exampleMSCCLInput variable below
+def parse_msccl_xml(xmlContent):
+    # parse the xmlContent
+    root = ET.fromstring(xmlContent)
+    assert root.tag == "algo"
+    return root.attrib
+
 # Parse the output file
 # For each matching line in the file return size and time for out-of-place and in-place
+# param path: path to the file
+# param separator: separator between columns for the output
 # Code is based on based on https://github.com/microsoft/msccl-leaderboard/blob/main/generate_graphs.py#L48
-def parse_test_log(path):
+def parse_test_log(path, separator):
     # Example input
     # [1,0]<stdout>:#
     # [1,0]<stdout>:#                                                              out-of-place                       in-place
@@ -241,7 +288,7 @@ def parse_test_log(path):
                 # split the line into an array of strings were each string is separated by one or more spaces
                 lineArray = line.split()
                 # Create a common separated string with the size, first time columns
-                output.append(f"{lineArray[1]},{lineArray[6]},{lineArray[10]}")
+                output.append(f"{lineArray[1]}{separator}{lineArray[6]}{separator}{lineArray[10]}")
 
     return output
 
@@ -277,10 +324,59 @@ def parse_test_log_inout_separate(path):
 
     return output
 
+# Format the header row for the CSV output file
+# param: separator is the character used to separate the columns in the CSV file
+# Returns the string for the header row
+def format_header_row(separator) -> str:
+    header = ('Collective'
+        + separator + 'Filter'
+        + separator + 'TimeOfTest'
+        + separator + 'nGPUs'
+        + separator + 'Protocol'
+        + separator + 'Library'
+        + separator + 'NcclVersion'
+        + separator + 'Size'
+        + separator + 'Time_OutOfPlace'
+        + separator + 'Time_InPlace'
+        + separator + 'MSCCL_Algo_Name'
+        + separator + 'MSCCL_File'
+        + '\n')
+    return header
 
+# This is and example of a MSCCL input file
+# Based on sccl-presynth/sccl_presynth/Allgather.n16-DistributedRelayedSwitch.localDGX1.copies2-steps20-gurobisol-improve-1630536923_i8_scRemote1_IBContig_h5-noring.sccl.xml
+exampleMCCLInput = '''
+<algo name="Allgather(n=16)-DistributedRelayedSwitch(local=DGX1,copies=2)-steps=20-gurobisol-improve-1630536923" nchannels="8" nchunksperloop="128" proto="Simple" ngpus="16" coll="allgather" inplace="1">
+ <gpu id="0" i_chunks="8" o_chunks="128" s_chunks="0">
+    <tb id="0" send="-1" recv="1" chan="0">
+      <step s="0" type="r" srcbuf="o" srcoff="8" dstbuf="o" dstoff="8" cnt="1" depid="-1" deps="-1" hasdep="1"/>
+      <step s="1" type="r" srcbuf="o" srcoff="16" dstbuf="o" dstoff="16" cnt="1" depid="-1" deps="-1" hasdep="1"/>
+      <step s="2" type="r" srcbuf="o" srcoff="56" dstbuf="o" dstoff="56" cnt="1" depid="-1" deps="-1" hasdep="1"/>
+      <step s="3" type="r" srcbuf="o" srcoff="72" dstbuf="o" dstoff="72" cnt="1" depid="-1" deps="-1" hasdep="1"/>
+      <step s="4" type="r" srcbuf="o" srcoff="96" dstbuf="o" dstoff="96" cnt="1" depid="-1" deps="-1" hasdep="0"/>
+      <step s="5" type="r" srcbuf="o" srcoff="88" dstbuf="o" dstoff="88" cnt="1" depid="-1" deps="-1" hasdep="0"/>
+      <step s="6" type="r" srcbuf="o" srcoff="80" dstbuf="o" dstoff="80" cnt="1" depid="-1" deps="-1" hasdep="1"/>
+      <step s="7" type="r" srcbuf="o" srcoff="112" dstbuf="o" dstoff="112" cnt="1" depid="-1" deps="-1" hasdep="1"/>
+      <step s="8" type="r" srcbuf="o" srcoff="120" dstbuf="o" dstoff="120" cnt="1" depid="-1" deps="-1" hasdep="1"/>
+      <step s="9" type="r" srcbuf="o" srcoff="104" dstbuf="o" dstoff="104" cnt="1" depid="-1" deps="-1" hasdep="0"/>
+    </tb>
+    <tb id="1" send="-1" recv="1" chan="1">
+      <step s="0" type="r" srcbuf="o" srcoff="9" dstbuf="o" dstoff="9" cnt="1" depid="-1" deps="-1" hasdep="1"/>
+      <step s="1" type="r" srcbuf="o" srcoff="17" dstbuf="o" dstoff="17" cnt="1" depid="-1" deps="-1" hasdep="1"/>
+      <step s="2" type="r" srcbuf="o" srcoff="57" dstbuf="o" dstoff="57" cnt="1" depid="-1" deps="-1" hasdep="1"/>
+      <step s="3" type="r" srcbuf="o" srcoff="73" dstbuf="o" dstoff="73" cnt="1" depid="-1" deps="-1" hasdep="1"/>
+      <step s="4" type="r" srcbuf="o" srcoff="97" dstbuf="o" dstoff="97" cnt="1" depid="-1" deps="-1" hasdep="0"/>
+      <step s="5" type="r" srcbuf="o" srcoff="89" dstbuf="o" dstoff="89" cnt="1" depid="-1" deps="-1" hasdep="0"/>
+      <step s="6" type="r" srcbuf="o" srcoff="81" dstbuf="o" dstoff="81" cnt="1" depid="-1" deps="-1" hasdep="1"/>
+      <step s="7" type="r" srcbuf="o" srcoff="113" dstbuf="o" dstoff="113" cnt="1" depid="-1" deps="-1" hasdep="1"/>
+      <step s="8" type="r" srcbuf="o" srcoff="121" dstbuf="o" dstoff="121" cnt="1" depid="-1" deps="-1" hasdep="1"/>
+      <step s="9" type="r" srcbuf="o" srcoff="105" dstbuf="o" dstoff="105" cnt="1" depid="-1" deps="-1" hasdep="0"/>
+    </tb>
+  </gpu>
+</algo>
+'''
 
-
-# Multi-line string to be used as the contents of the testRun program
+# This is an example of output from the NCCL benchmark. This is used for testing.
 exampleOutput = '''
 [1,5]<stdout>:az-eus-v100-32gb-5-worker-zphjiy:18101:18135 [5] NCCL INFO comm 0x7f3804000fa0 rank 5 nranks 16 cudaDev 5 busId 3bfd00000 - Init COMPLETE
 [1,0]<stdout>:NCCL version 2.12.12.MSCCL.0.7.3+cuda11.6
